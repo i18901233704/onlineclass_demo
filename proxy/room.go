d5d379f633de54ff3f21b9d1b35cb2e68dc8a2e5
lizhengping <lizhengping@51talk.com>
2020-12-30 20:05:04 +0800

    first commit

---
+++ proxy/room.go
@@ -0,0 +1,174 @@
+package proxy
+
+import(
+	zmq "github.com/pebbe/zmq4"
+	"sync"
+)
+
+
+var(
+
+	roomMsgChan = make(chan []byte,10000 * 10)
+	InnerPubAddr string = "inproc://"
+)
+
+
+type Room struct {
+
+	Roomid string
+	lock sync.RWMutex 
+	userlist map[string]*Client
+
+	insubscriber *zmq.Socket
+	subscriber *zmq.Socket
+	dealer     *zmq.Socket
+	poller   *zmq.Poller
+}
+
+type RoomManager struct{
+	lock sync.RWMutex
+	roomlist map[string]*Room
+}
+
+func (m *RoomManager) RecvFromChanLoop(){
+	publisher, err := zmq.NewSocket(zmq.PUB)
+	defer publisher.Close()
+	InnerPubAddr := fmt.Sprintf("%v%v",InnerPubAddr,time.Now().Format("2006-01-02:15:04:05"))
+	err = publisher.Bind(InnerPubAddr)
+	if err != nil{
+		fmt.Println("publisher bind:",err)
+		os.Exit(1)
+	}
+
+	for msg := range roomMsgChan {
+		publisher.SendMessage(msg)
+	}
+
+}
+
+func (m *RoomManager) CreateRoom (roomid string){
+	m.lock.Lock()
+	_, ok := m.roomlist[roomid]
+	if ok {
+		return
+	}
+	 
+	room := &Room{Roomid: roomid, userlist: make(map[string]*Client)}
+	m.roomlist[roomid] =room
+	m.lock.Unlock()
+
+	go room.SendAndRecvLoop()
+}
+
+func (m *RoomManager) DeleteRoom(roomid string) {
+	m.lock.Lock()
+	room ,ok := m.roomlist[roomid]
+	delete(m.roomlist,roomid)
+	m.lock.Unlock()
+	if ok && room != nil{
+		room.Destroy()
+	}
+}
+
+func (r *Room) AddUser(userid string, c *Client) {
+	r.lock.Lock()
+	u, ok := r.userlist[userid]
+	r.userlist[userid] = c
+	r.lock.Unlock()
+
+	if ok && u !=nil {
+		u.SelfKickoff()	
+	}
+}
+
+func (r * Room) Destroy(){
+	r.lock.Lock()
+	ulist := []*Client
+	for u ,c := range r.userlist{
+		ulist= append(ulist,c)
+	}
+	r.lock.Unlock()
+
+	for u := range ulist{
+		u.Destroy()
+	}
+}
+
+func (r * Room) DelUser(userid string){
+	r.lock.Lock()
+	u,ok := r.userlist[userid]
+	delete(r.userlist, userid)
+	r.lock.Unlock()
+
+	if ok && u != nil{
+		u.Destroy()
+	}
+
+}
+
+func (r *Room) ConnectInPub(){
+	r.insubscriber, _ := zmq.NewSocket(zmq.SUB)
+	r.insubscriber.Connect(InnerPubAddr)
+	r.insubscriber.SetSubscribe(r.Roomid)
+	r.poller.Add(r.insubscriber, zmq.POLLIN)
+}
+
+func (r *Room) ConnectPub(){
+	r.subscriber, _ := zmq.NewSocket(zmq.SUB)
+	r.subscriber.Connect("tcp://127.0.0.1:8082")
+	r.subscriber.SetSubscribe(r.Roomid)
+	r.poller.Add(r.subscriber, zmq.POLLIN)
+}
+
+func (r *Room) ConnectRouter(){
+	r.dealer, _ := zmq.NewSocket(zmq.SUB)
+	r.dealer.Connect("tcp://127.0.0.1:8081")
+	r.poller.Add(r.dealer, zmq.POLLIN)
+}
+
+func (r *Room) SendAndRecvLoop(){
+
+	r.poller := zmq.NewPoller()
+	r.ConnectPub()
+	r.ConnectRouter()
+	r.ConnectInPub()
+
+	for {
+        sockets, _ := poller.Poll(-1)
+        for _, socket := range sockets {
+            switch s := socket.Socket; s {
+            case r.insubscriber:
+                msg, err := s.RecvMessage(0)
+                if err != nil{
+                	r.ProcessInSub(msg)
+                }
+            case r.subscriber:
+                msg, err := s.RecvMessage(0)
+                if err != nil{
+                	r.ProcessSub(msg)
+                }
+            }
+
+            case r.dealer:
+                msg, err := s.RecvMessage(0)
+                if err != nil{
+                	r.ProcessDealer(msg)
+                }
+            
+        }
+    }
+
+}
+
+
+func (r *Room) ProcessInSub(msg []string){
+
+}
+
+func (r *Room) ProcessSub(msg []string){
+	
+}
+
+func (r *Room) ProcessDealer(msg []string){
+	
+}
---
+++ proxy/websocket.go
@@ -0,0 +1,154 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package proxy
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"time"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/websocket"
+)
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 1 * time.Second
+
+	// Time allowed to read the next message from the peer.
+	pingWait = 60 * time.Second
+
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+// Client is a middleman between the websocket connection and the proxy.
+type Client struct {
+
+	Roomid string
+
+	Userid string
+	// The websocket connection.
+	conn *websocket.Conn
+
+	// Buffered channel of outbound messages.
+	send chan []byte
+}
+
+func (c *Client) LeaveRoom(){
+
+	msg := map[string]string{}
+	msg["cmd"] = "leave"
+	msg["roomid"] = c.Roomid
+	msg["userid"] = c.Userid
+	msg["cli_timestamp"] = fmt.Sprintf("%v-%v"time.Now().UnixNano() / 1e6,1)
+	data,_ := json.Marshal(msg)
+	select {
+		case roomMsgChan <- data :
+			fmt.Println("ok leave and broadcast", string(data))
+		default:
+			fmt.Println("error leave not broadcast", string(data))
+
+	}
+}
+
+func (c *Client) SendToRouter(msg byte[]){
+
+	fmt.Println("recv message from client and send to  roomMsgChan",string(msg))
+	select {
+		case roomMsgChan <- msg:
+			fmt.Println("send to roomMsgChan ok",string(msg))
+		default:
+			fmt.Println("send to roomMsgChan error",string(msg))
+	}
+}
+
+func (c *Client) ReadMessage() {
+	defer func() {
+		c.Conn.Close()
+		close(c.send)
+		c.LeaveRoom()
+	}()
+
+	c.conn.SetReadDeadline(time.Now().Add(pingWait))
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		msg := map[string]string{}
+		json.UnMarshal(&msg,message)
+		if msg["cmd"] == "enter" {
+			
+		}
+
+		c.SendToRouter(message)
+	}
+}
+
+
+func (c *Client) SendToClient(msg []byte) {
+
+	fmt.Println("recv message from room and send to client",string(msg))
+	select {
+		case c.send <- msg:
+			fmt.Println("send to c.send ok",string(msg))
+		default:
+			fmt.Println("send to c.send error",string(msg))
+	}
+
+}
+
+func (c *Client) WriteMessage() {
+
+	for {
+		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				// The hub closed the channel.
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			err := c.conn.WriteMessage(websocket.TextMessage, message)
+			if err !=nil{
+				fmt.Println("send message to client  ok",string(message))
+			} else{
+				fmt.Println("send message to client  error",string(message))
+			}
+		}
+	}
+}
+
+// serveWs handles websocket requests from the peer.
+func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	client := &Client{conn: conn, send: make(chan []byte, 256)}
+
+	// Allow collection of memory referenced by the caller by doing all work in
+	// new goroutines.
+	go client.WriteMessage()
+	go client.ReadMessage()
+}
